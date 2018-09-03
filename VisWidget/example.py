6a0a1309a46b239ae9157b342eb2d8071577f387
Kyle Harrison <kyleharrison94@hotmail.com>
2018-09-03 16:39:26 +0200

    Callbacks to python (print) working from click

--- VisWidget/example.py
+++ VisWidget/example.py
@@ -1,5 +1,7 @@
 import ipywidgets as widgets
-from traitlets import Unicode
+from traitlets import Unicode, Int, List
+from traitlets import observe
+import matplotlib.pyplot as plt
 
 @widgets.register
 class HelloWorld(widgets.DOMWidget):
@@ -10,4 +12,10 @@ class HelloWorld(widgets.DOMWidget):
     _model_module = Unicode('VisWidget').tag(sync=True)
     _view_module_version = Unicode('^0.1.0').tag(sync=True)
     _model_module_version = Unicode('^0.1.0').tag(sync=True)
-    value = Unicode('Hello!').tag(sync=True)
+    index = Int(0).tag(sync=True)
+    #rawdata = List([],[]).tag(sync=True)
+    #xyz = List([],[],[]).tag(sync=True)
+
+    @observe('index')
+    def val_changed(self, change):
+        print(change['new'])
--- js/lib/example.js
+++ js/lib/example.js
@@ -25,7 +25,7 @@ var HelloModel = widgets.DOMWidgetModel.extend({
         _view_module : 'VisWidget',
         _model_module_version : '0.1.0',
         _view_module_version : '0.1.0',
-        value : 'World'
+        index : 1
     })
 });
 
@@ -33,51 +33,73 @@ var HelloModel = widgets.DOMWidgetModel.extend({
 // Custom View. Renders the widget model.
 var HelloView = widgets.DOMWidgetView.extend({
     render: function () {
-        console.log("Creating html");
-        // this.value_changed();
-        // this.model.on('change:value', this.value_changed, this);
+        var model = this.model;
+        var that = this;
 
         var data = null;
         var graph = null;
-        // Called when the Visualization API is loaded.
-            // create the data table.
-            data = new vis.DataSet();
-            // create some shortcuts to math functions
-            var sqrt = Math.sqrt;
-            var pow = Math.pow;
-            var random = Math.random;
-            // create the animation data
-            var imax = 100;
-            for (var i = 0; i < imax; i++) {
-                var x = pow(random(), 2);
-                var y = pow(random(), 2);
-                var z = pow(random(), 2);
-                var style = (i % 2 == 0) ? sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)) : "#00ffff";
-                data.add({ x: x, y: y, z: z, style: style });
-            }
-            // specify options
-            var options = {
-                width: '600px',
-                height: '600px',
-                style: 'dot-color',
-                showPerspective: true,
-                showGrid: true,
-                keepAspectRatio: true,
-                verticalRatio: 1.0,
-                legendLabel: 'distance',
-                cameraPosition: {
-                    horizontal: -0.35,
-                    vertical: 0.22,
-                    distance: 1.8
+        data = new vis.DataSet();
+        var sqrt = Math.sqrt;
+        var pow = Math.pow;
+        var random = Math.random;
+
+
+        // create the animation data
+        var imax = 30;
+        for (var i = 0; i < imax; i++) {
+            var x = pow(random(), 2);
+            var y = pow(random(), 2);
+            var z = pow(random(), 2);
+            var style = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+            data.add({ x: x, y: y, z: z, style: style, id: i });
+        }
+        // specify options
+        var options = {
+            width: '600px',
+            height: '600px',
+            style: 'dot-color',
+            showPerspective: true,
+            showGrid: true,
+            keepAspectRatio: true,
+            verticalRatio: 1.0,
+            onclick: (point) => {
+                console.log(point);
+                this.model.set('index', point.id);
+                this.touch();
+            },
+            legendLabel: 'distance',
+            cameraPosition: {
+                horizontal: -0.35,
+                vertical: 0.22,
+                distance: 1.8
+            },
+            tooltip: function (point) {
+                //that.model.set('index', point.data.id);
+                //that.touch();
+                return "<b>ID:</b> " + point.data.id + "<br>"
+                    + "<b>Label:</b> " + point.data.style.toFixed(3) + "<br>"
+                    + "<b>X:</b> " + point.x.toFixed(3) + " <b>Y: </b>" + point.y.toFixed(3) + " <b>Z: </b>" + point.z.toFixed(3);
+            },
+            tooltipStyle: {
+                content: {
+                    background: 'rgba(255, 255, 255, 0.9)',
+                    padding: '10px',
+                    borderRadius: '10px'
+                },
+                line: {
+                    height: '20px',
+                    width: '0',
+                    borderLeft: '1px hidden #4d4d4d'
+                },
+                dot: {
+                    border: '0px hidden rgba(0, 0, 0, 0.5)'
                 }
             }
-            graph = new vis.Graph3d(this.el, data, options);
+        }
+        graph = new vis.Graph3d(this.el, data, options);
 
     },
 
-    value_changed: function() {
-       // this.el.textContent = this.model.get('value');
-    }
 });
 
 
